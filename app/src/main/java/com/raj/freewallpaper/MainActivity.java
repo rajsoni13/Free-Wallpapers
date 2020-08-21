286888a9ff822443aaf1691acdb4d11042e1297d
hirenpokar <46025100+hirenpokar@users.noreply.github.com>
2020-08-21 11:46:41 +0530

    - layouts are set. - Images of categories. and more.
    Task(Raj):
    - Set Strings of categories like images(dynamically) using adapter or whatever and display in textview.

--- app/src/main/java/com/raj/freewallpaper/MainActivity.java
+++ app/src/main/java/com/raj/freewallpaper/MainActivity.java
@@ -1,17 +1,40 @@
 package com.raj.freewallpaper;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.widget.GridView;
-
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     GridView gridView;
-    int imgData[] = {R.mipmap.ic_launcher, R.drawable.ic_java, R.drawable.ic_ios,
-            R.drawable.ic_php, R.drawable.ic_c, R.drawable.ic_cc, R.drawable.ic_python};
     ArrayList<LangModel> langModelArrayList;
+
+    int[] imgData = {
+            R.drawable.beach,
+            R.drawable.bikes,
+            R.drawable.cars,
+            R.drawable.minimalist,
+            R.drawable.setup,
+            R.drawable.studio,
+            R.drawable.sunset,
+            R.drawable.space,
+            R.drawable.ab,
+            R.drawable.mountains
+    };
+
+    int[] strData = {
+            R.string.beach,
+            R.string.bikes,
+            R.string.cars,
+            R.string.minimalist,
+            R.string.setup,
+            R.string.studio,
+            R.string.sunset,
+            R.string.space,
+            R.string.ab,
+            R.string.mountains,
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,16 +42,12 @@ public class MainActivity extends AppCompatActivity {
 
         gridView= (GridView) findViewById(R.id.grdview);
         langModelArrayList = new ArrayList<LangModel>();
-        for (int i = 0; i < imgData.length; i++) {
-
-
-            LangModel langModel = new LangModel(imgData[i]);
-
+        for (int imgDatum : imgData) {
+            LangModel langModel = new LangModel(imgDatum);
             langModelArrayList.add(langModel);
         }
 
-        MyBaseAdapter myBaseAdapter = new
-                MyBaseAdapter(this, langModelArrayList);
+        MyBaseAdapter myBaseAdapter = new MyBaseAdapter(this, langModelArrayList);
         gridView.setAdapter(myBaseAdapter);
 
     }
